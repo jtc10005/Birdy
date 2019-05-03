4670111468a1628b0ab603aaa0c3761f5ee9e9a1
jtc10005 <jtc10005@gmail.com>
2019-05-02 19:34:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-o5s9wk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-o5s9wk)
---
+++ app/Containers/log.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'log-component',
+  templateUrl: './log.component.html',
+  styleUrls: [ '../app.component.css' ]
+})
+export class LogComponent  {
+}
